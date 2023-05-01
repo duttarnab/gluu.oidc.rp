56919d645ce748eb8eed0d187602bb1af808664c
Arnab Dutta <arnab.bdutta@gmail.com>
2023-05-01 23:20:37 +0530

    feat: adding logout page

--- src/background.js
+++ src/background.js
@@ -2,94 +2,95 @@
 const fetch = require('node-fetch');
 //import './node-bundle.js';
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.type == "register_click_event") {
-    console.log("click event captured in current webpage");
-    // Call the callback passed to chrome.action.onClicked
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+   if (request.type == "register_click_event") {
+      console.log("click event captured in current webpage");
+      // Call the callback passed to chrome.action.onClicked
 
-    getOpenidConfiguration(request.issuer).then(openapiConfig => {
+      getOpenidConfiguration(request.issuer).then(openapiConfig => {
 
-       if(openapiConfig != undefined) {
-          try {
-             JSON.parse(openapiConfig)
-          } catch( err ) {
-             console.error(err)
-             sendResponse({ result: "error", message: "Error in registration!" });
-          }
-          chrome.storage.local.set({ opConfiguration: openapiConfig }).then(() => 
-             {console.log("openapiConfig is set to " + openapiConfig);
-          });
-          
-          const registrationUrl = JSON.parse(openapiConfig).registration_endpoint
- 
-          registerOIDCClient(registrationUrl, request.registerObj).then(registrationResp => {
+         if (openapiConfig != undefined) {
+            try {
+               JSON.parse(openapiConfig)
+            } catch (err) {
+               console.error(err)
+               sendResponse({ result: "error", message: "Error in registration!" });
+            }
+            chrome.storage.local.set({ opConfiguration: openapiConfig }).then(() => {
+               console.log("openapiConfig is set to " + openapiConfig);
+            });
 
-          if(registrationResp != undefined) {
-             
-             chrome.storage.local.set({ 
-                oidcClient: {'op_host': request.issuer,
-                   'client_id': registrationResp.client_id, 
-                   'client_secret': registrationResp.client_secret,
-                   'scope': request.registerObj.scope,
-                   'redirect_uri': request.registerObj.redirect_uris,
-                   'acr_values': request.registerObj.default_acr_values,
-                   'authorization_endpoint': JSON.parse(openapiConfig).authorization_endpoint,
-                   'response_type': request.registerObj.response_types,
-                   'additionalParams': request.registerObj.additionalParam
-                }}).then(async () => 
-             {
-                   console.log("oidcClient is set for client_id: " + registrationResp.client_id);
-                   sendResponse({ result: "success", message: "Regstration successful!" });
+            const registrationUrl = JSON.parse(openapiConfig).registration_endpoint
 
-             });
-             console.log('OIDC client registered successfully!')
-             
-          } else {
-             sendResponse({ result: "error", message: "Error in registration!" });
-          }
-       });
-       } else {
-          sendResponse({ result: "error", message: "Error in registration!" });
-       }         
-    });
-    return true;
-    console.log("click event captured in current webpage...end");
-    //chrome.windows.create({url: "https://admin-ui-test.gluu.org/jans-auth/authorize.htm?scope=openid+profile+user_name+email&acr_values=basic&response_type=code&redirect_uri=https%3A%2F%2Fadmin-ui-test.gluu.org%2Fadmin&state=07adc860-5ce8-4516-a15f-65f8c5b9a882&nonce=bf92bede-c1c4-43da-9cb0-83b14ecdb467&client_id=2001.bfd15f73-96cf-4ac7-a066-32c78d516c16",focused: true, incognito: true});
-  }
+            registerOIDCClient(registrationUrl, request.registerObj).then(registrationResp => {
+
+               if (registrationResp != undefined) {
+
+                  chrome.storage.local.set({
+                     oidcClient: {
+                        'op_host': request.issuer,
+                        'client_id': registrationResp.client_id,
+                        'client_secret': registrationResp.client_secret,
+                        'scope': request.registerObj.scope,
+                        'redirect_uri': request.registerObj.redirect_uris,
+                        'acr_values': request.registerObj.default_acr_values,
+                        'authorization_endpoint': JSON.parse(openapiConfig).authorization_endpoint,
+                        'response_type': request.registerObj.response_types,
+                        'additionalParams': request.registerObj.additionalParam
+                     }
+                  }).then(async () => {
+                     console.log("oidcClient is set for client_id: " + registrationResp.client_id);
+                     sendResponse({ result: "success", message: "Regstration successful!" });
+
+                  });
+                  console.log('OIDC client registered successfully!')
+
+               } else {
+                  sendResponse({ result: "error", message: "Error in registration!" });
+               }
+            });
+         } else {
+            sendResponse({ result: "error", message: "Error in registration!" });
+         }
+      });
+      return true;
+      console.log("click event captured in current webpage...end");
+      //chrome.windows.create({url: "https://admin-ui-test.gluu.org/jans-auth/authorize.htm?scope=openid+profile+user_name+email&acr_values=basic&response_type=code&redirect_uri=https%3A%2F%2Fadmin-ui-test.gluu.org%2Fadmin&state=07adc860-5ce8-4516-a15f-65f8c5b9a882&nonce=bf92bede-c1c4-43da-9cb0-83b14ecdb467&client_id=2001.bfd15f73-96cf-4ac7-a066-32c78d516c16",focused: true, incognito: true});
+   }
 });
 
 
 async function registerOIDCClient(registration_endpoint, registerObj) {
- try {
-    const response = await fetch(registration_endpoint, {
-     method: "POST", // *GET, POST, PUT, DELETE, etc.
-     mode: "cors", // no-cors, *cors, same-origin
-     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-     credentials: "same-origin", // include, *same-origin, omit
-     headers: {
-       "Content-Type": "application/json"
-     },
-     redirect: "follow", // manual, *follow, error
-     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-     body: JSON.stringify(registerObj), // body data type must match "Content-Type" header
-    });
-    return await response.json();
+   try {
+      const response = await fetch(registration_endpoint, {
+         method: "POST", // *GET, POST, PUT, DELETE, etc.
+         mode: "cors", // no-cors, *cors, same-origin
+         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+         credentials: "same-origin", // include, *same-origin, omit
+         headers: {
+            "Content-Type": "application/json"
+         },
+         redirect: "follow", // manual, *follow, error
+         referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+         body: JSON.stringify(registerObj), // body data type must match "Content-Type" header
+      });
+      return await response.json();
 
- } catch(err) {
-    console.error(err)
- }
+   } catch (err) {
+      console.error(err)
+   }
 
 }
 
 async function getOpenidConfiguration(issuer) {
- try {
+   try {
 
-    const endpoint = issuer + '/.well-known/openid-configuration';
-    const res = await fetch(endpoint);
-    return await res.text();
- } catch(err) {
-    console.error(err)
- }
+      const endpoint = issuer + '/.well-known/openid-configuration';
+      const res = await fetch(endpoint);
+      return await res.text();
+   } catch (err) {
+      console.error(err)
+   }
 
 }
 
--- src/popup.js
+++ src/popup.js
@@ -1,200 +1,286 @@
 'use strict';
 
 import './popup.css';
-
+const axios = require('axios');
+const qs = require('qs');
 (function () {
-  
-  checkDB();
+
+  onLoad();
+
+  async function onLoad() {
+    
+    const loginDetails = await new Promise((resolve, reject) => {
+      chrome.storage.local.get(["loginDetails"], (result) => {
+        resolve(JSON.stringify(result));
+      });
+    });
+    console.log('loginDetails: '+JSON.stringify(loginDetails))
+    if (loginDetails && Object.keys(loginDetails).length !== 0) {
+      await trigCodeFlowButton()
+    }
+    checkDB()
+  }
 
   function checkDB() {
-     chrome.storage.local.get(["oidcClient"]).then((result) => {
-        if(result.oidcClient != undefined) {
-             console.log("Value currently is " + JSON.stringify(result.oidcClient));
-            
-             document.getElementById('opHost').value = result.oidcClient.op_host
-             document.getElementById('clientId').value = result.oidcClient.client_id
-             document.getElementById('clientSecret').value = result.oidcClient.client_secret
-  
-             document.getElementById('registerForm').style.display = "none";
-             document.getElementById('oidcClientDetails').style.display = "block";
-        } else {
-           document.getElementById('registerForm').style.display = "block";
-           document.getElementById('oidcClientDetails').style.display = "none";
-        }
-     });
-  }
-     
-  async function resetClient() {
-     chrome.storage.local.remove(["oidcClient","opConfiguration"],function(){
-      var error = chrome.runtime.lastError;
-         if (error) {
-             console.error(error);
-         } else {
-            checkDB();
-         }
-     });
-  }
-  
-  async function trigCodeFlowButton() {
     chrome.storage.local.get(["oidcClient"]).then((result) => {
-      if(result.oidcClient != undefined) {
-        let authzUrl =  result.oidcClient.authorization_endpoint +
-                        '?scope=' + result.oidcClient.scope[0] + 
-                        '&acr_values=' + result.oidcClient.acr_values[0] + 
-                        '&response_type=' + result.oidcClient.response_type[0] + 
-                        '&redirect_uri=' + result.oidcClient.redirect_uri[0] + 
-                        '&client_id=' + result.oidcClient.client_id + 
-                        '&state=' + uuidv4() + 
-                        '&nonce=' + uuidv4();
-      
-      let additionalParams = result.oidcClient.additionalParams
-  
-      if(additionalParams != undefined && additionalParams.trim() != '') {
-        let additionalParamJSON = JSON.parse(additionalParams)
-        Object.keys(additionalParamJSON).forEach(key => {
-        
-          console.log(key+"~~~"+ additionalParamJSON[key]);
-        authzUrl +=  `&${key}=${additionalParamJSON[key]}`
-      });
-      }                
-        chrome.windows.create({url: authzUrl,focused: true, incognito: true});
+      if (result.oidcClient != undefined) {
+        console.log("Value currently is " + JSON.stringify(result.oidcClient));
+
+        document.getElementById('opHost').value = result.oidcClient.op_host
+        document.getElementById('clientId').value = result.oidcClient.client_id
+        document.getElementById('clientSecret').value = result.oidcClient.client_secret
+
+        document.getElementById('registerForm').style.display = "none";
+        document.getElementById('oidcClientDetails').style.display = "block";
+      } else {
+        document.getElementById('registerForm').style.display = "block";
+        document.getElementById('oidcClientDetails').style.display = "none";
       }
     });
   }
-  function saveExistingClientDetailsInDB() {
-   var existingClientId = document.getElementById('existingClientId').value
-   var issuer = document.getElementById('issuer').value
-   var redirectUri = document.getElementById('redirectUri').value
-   var acrValues = document.getElementById('acrValues').value
-   var additionalParam = document.getElementById('additionalParam').value
-   var scope = document.getElementById('scope').value
-   var authzUrl = document.getElementById('authzUrl').value
 
-   chrome.storage.local.set({ 
-      oidcClient: {'op_host': issuer,
-         'client_id': existingClientId, 
-         'client_secret': '----------',
-         'scope': [scope],
-         'redirect_uri': [redirectUri],
-         'acr_values': [acrValues],
-         'authorization_endpoint': authzUrl,
-         'response_type': ['code'],
-         'additionalParams': additionalParam
-      }}).then(async () => 
-      {
-         console.log("oidcClient is set for client_id: " + existingClientId);
-         checkDB();
-
-      });
-  }
-  async function submitForm() {
-    
-     if (validateForm()) {
-     var existingClientId = document.getElementById('existingClientId').value
-     if(existingClientId.trim() != '') {
-      saveExistingClientDetailsInDB();
-        return;
-     }
-     var issuer = document.getElementById('issuer').value
-     var redirectUri = document.getElementById('redirectUri').value
-     var acrValues = document.getElementById('acrValues').value
-     var additionalParam = document.getElementById('additionalParam').value
-     var scope = document.getElementById('scope').value
-  
-     var registerObj = {}
-     registerObj.issuer = issuer
-  
-     registerObj.redirect_uris =  [redirectUri]
-     registerObj.default_acr_values = [acrValues]
-     registerObj.additionalParam = additionalParam
-     registerObj.scope = [scope]
-  
-     registerObj.response_types = ['code']
-     registerObj.grant_types = ['authorization_code']
-     registerObj.application_type = 'web'
-     registerObj.client_name = 'Gluu-RP-' + uuidv4()
-     registerObj.token_endpoint_auth_method = 'client_secret_post'
-    try{
-       const response = await register(issuer, registerObj)
-   
-      if(response.result == 'success') {
-              checkDB();
-        } else {
-              document.getElementById('errorSpanTop').innerHTML = 'Error in registration.'
-              document.getElementById('errorSpanBot').innerHTML = 'Error in registration.'
-        }
-  
-     } catch(err) {
-        console.error(err)
-     }
-       
+  async function resetClient() {
+    chrome.storage.local.remove(["oidcClient", "opConfiguration"], function () {
+      var error = chrome.runtime.lastError;
+      if (error) {
+        console.error(error);
+      } else {
+        checkDB();
       }
+    });
   }
-  
-  async function register(issuer, registerObj){
-     return await new Promise(resolve => {
-        chrome.runtime.sendMessage({
-           type: "register_click_event",
-           issuer: issuer,
-           registerObj: registerObj
-         },resolve);
+
+  async function trigCodeFlowButton() {
+
+    const redirectUrl = /*chrome.runtime.getURL('redirect.html')*/chrome.identity.getRedirectURL()
+    console.log('redirectUrl', redirectUrl)
+    chrome.storage.local.get(["oidcClient"]).then(async (result) => {
+      if (result.oidcClient != undefined) {
+        let authzUrl = result.oidcClient.authorization_endpoint +
+          '?scope=' + `${result.oidcClient.scope[0]}+profile` +
+          '&acr_values=' + result.oidcClient.acr_values[0] +
+          '&response_type=' + result.oidcClient.response_type[0] +
+          '&redirect_uri=' + redirectUrl +//result.oidcClient.redirect_uri[0] + 
+          '&client_id=' + result.oidcClient.client_id +
+          '&state=' + uuidv4() +
+          '&nonce=' + uuidv4();
+
+        let additionalParams = result.oidcClient.additionalParams
+
+        if (additionalParams != undefined && additionalParams.trim() != '') {
+          let additionalParamJSON = JSON.parse(additionalParams)
+          Object.keys(additionalParamJSON).forEach(key => {
+
+            console.log(key + "~~~" + additionalParamJSON[key]);
+            authzUrl += `&${key}=${additionalParamJSON[key]}`
+          });
+        }
+
+
+        const resultUrl = await new Promise((resolve, reject) => {
+          chrome.identity.launchWebAuthFlow({
+            url: authzUrl,
+            interactive: true
+          }, callbackUrl => {
+            console.log('callbackUrl', callbackUrl)
+            resolve(callbackUrl);
+          });
         });
+
+        //chrome.windows.create({url: redirectUrl,focused: true, incognito: true});
+
+        if (resultUrl) {
+
+          //const code = parse(resultUrl.split('?')[1]).code;
+          const urlParams = new URLSearchParams(new URL(resultUrl).search)
+          const code = urlParams.get('code')
+          console.log('code:' + code)
+          const opConfig = await new Promise((resolve, reject) => {
+            chrome.storage.local.get(["opConfiguration"], (result) => {
+              //alert(JSON.stringify(result))
+              resolve(JSON.stringify(result));
+            });
+          });
+
+          console.log(JSON.parse(JSON.parse(opConfig).opConfiguration).token_endpoint)
+
+          const tokenReqData = qs.stringify({
+            redirect_uri: redirectUrl,
+            grant_type: 'authorization_code',
+            client_id: result.oidcClient.client_id,
+            code,
+            scope: result.oidcClient.scope[0]
+          })
+
+          const tokenReqOptions = {
+            method: 'POST',
+            headers: { 'content-type': 'application/x-www-form-urlencoded', 'Authorization': 'Basic ' + btoa(`${result.oidcClient.client_id}:${result.oidcClient.client_secret}`) },
+            data: tokenReqData,
+            url: JSON.parse(JSON.parse(opConfig).opConfiguration).token_endpoint,
+          };
+
+          const tokenResponse = await axios(tokenReqOptions);
+
+
+          if (
+            tokenResponse &&
+            tokenResponse.data &&
+            tokenResponse.data.access_token
+          ) {
+            console.log('tokenResponse:' + JSON.stringify(tokenResponse))
+            const userInfoData = qs.stringify({
+              access_token: tokenResponse.data.access_token
+            })
+            const userInfoOptions = {
+              method: 'POST',
+              headers: { 'content-type': 'application/x-www-form-urlencoded', 'Authorization': `Bearer ${tokenResponse.data.access_token}` },
+              data: userInfoData,
+              url: JSON.parse(JSON.parse(opConfig).opConfiguration).userinfo_endpoint,
+            };
+
+            const userInfoResponse = await axios(userInfoOptions);
+            console.log('userInfoResponse:' + JSON.stringify(userInfoResponse))
+
+            chrome.storage.local.set({
+              loginDetails: {
+                'access_token': tokenResponse.data.access_token,
+                'userDetails': userInfoResponse.data,
+              }
+            }).then(async () => {
+              console.log("userDetails: " + userInfoResponse.data);
+              document.getElementById('userDetailsSpan').innerHTML = JSON.stringify(userInfoResponse.data)
+              document.getElementById('userDetailsDiv').style.display = "block";
+              document.getElementById('registerForm').style.display = "none";
+              document.getElementById('oidcClientDetails').style.display = "none";
+
+            });
+
+            //logout();
+          }
+        }
+      }
+    });
   }
-  
-  
+
+  function logout() {
+    chrome.identity.clearAllCachedAuthTokens(() => {
+
+      chrome.storage.local.remove(["loginDetails"], function () {
+        var error = chrome.runtime.lastError;
+        if (error) {
+          console.error(error);
+        } else {
+          document.getElementById('userDetailsSpan').innerHTML = ''
+          document.getElementById('userDetailsDiv').style.display = "none";
+          document.getElementById('registerForm').style.display = "none";
+          document.getElementById('oidcClientDetails').style.display = "block";
+          axios.get(`https://admin-ui-test.gluu.org/jans-auth/restv1/end_session?state=${uuidv4()}`)
+        }
+      });
+
+    });
+
+  }
+
+  async function submitForm() {
+
+    if (validateForm()) {
+
+      var issuer = document.getElementById('issuer').value
+      var acrValues = document.getElementById('acrValues').value
+      var additionalParam = document.getElementById('additionalParam').value
+      var scope = document.getElementById('scope').value
+
+      var registerObj = {}
+      registerObj.issuer = issuer
+
+      registerObj.redirect_uris =  /*[chrome.runtime.getURL('redirect.html')]*/[chrome.identity.getRedirectURL()]//[redirectUri]
+      registerObj.default_acr_values = [acrValues]
+      registerObj.additionalParam = additionalParam
+      registerObj.scope = [scope, 'profile']
+
+      registerObj.response_types = ['code']
+      registerObj.grant_types = ['authorization_code', 'client_credentials']
+      registerObj.application_type = 'web'
+      registerObj.client_name = 'Gluu-RP-' + uuidv4()
+      registerObj.token_endpoint_auth_method = 'client_secret_basic'
+      try {
+        const response = await register(issuer, registerObj)
+
+        if (response.result == 'success') {
+          checkDB();
+        } else {
+          document.getElementById('errorSpanTop').innerHTML = 'Error in registration.'
+          document.getElementById('errorSpanBot').innerHTML = 'Error in registration.'
+        }
+
+      } catch (err) {
+        console.error(err)
+      }
+
+    }
+  }
+
+  async function register(issuer, registerObj) {
+    return await new Promise(resolve => {
+      chrome.runtime.sendMessage({
+        type: "register_click_event",
+        issuer: issuer,
+        registerObj: registerObj
+      }, resolve);
+    });
+  }
+
+
   function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
       (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
   }
-  
-  function validateForm()  {
-     document.getElementById('errorSpanTop').innerHTML = ''
-     document.getElementById('errorSpanBot').innerHTML = ''
-     var issuer = document.getElementById('issuer').value
-     var redirectUri = document.getElementById('redirectUri').value
-     var acrValues = document.getElementById('acrValues').value
-     var additionalParam = document.getElementById('additionalParam').value
-     var scope = document.getElementById('scope').value
-  
-     var emptyField = ''
-     if(issuer.trim() == '') {
-        emptyField += 'issuer '
-        
-     }
-     if(redirectUri.trim() == '') {
-        emptyField += 'redirect_uri '
-        
-     }
-     if(acrValues.trim() == '') {
-        emptyField += 'acr_values '
-        
-     }   
-     if(scope.trim() == '') {
-        emptyField += 'scope '
-        
-     }  
-     if(emptyField.trim() != '') {
-        document.getElementById('errorSpanTop').innerHTML = '<b>The following fields are mandatory</b>: ' + emptyField
-        document.getElementById('errorSpanBot').innerHTML = '<b>The following fields are mandatory</b>: ' + emptyField
-        return false;
-     }
-     return true;
+
+  function validateForm() {
+    document.getElementById('errorSpanTop').innerHTML = ''
+    document.getElementById('errorSpanBot').innerHTML = ''
+    var issuer = document.getElementById('issuer').value
+    var acrValues = document.getElementById('acrValues').value
+    var additionalParam = document.getElementById('additionalParam').value
+    var scope = document.getElementById('scope').value
+
+    var emptyField = ''
+    if (issuer.trim() == '') {
+      emptyField += 'issuer '
+
+    }
+
+    if (acrValues.trim() == '') {
+      emptyField += 'acr_values '
+
+    }
+    if (scope.trim() == '') {
+      emptyField += 'scope '
+
+    }
+    if (emptyField.trim() != '') {
+      document.getElementById('errorSpanTop').innerHTML = '<b>The following fields are mandatory</b>: ' + emptyField
+      document.getElementById('errorSpanBot').innerHTML = '<b>The following fields are mandatory</b>: ' + emptyField
+      return false;
+    }
+    return true;
   }
-  
+
   function handleClick() {
-   if(document.getElementById('clientExists').checked) {
+    if (document.getElementById('clientExists').checked) {
       document.getElementById('existingClientIdDiv').style.display = "block";
-   } else {
+    } else {
       document.getElementById('existingClientIdDiv').style.display = "none";
-   }
+    }
   }
-  
+
   document.addEventListener('DOMContentLoaded', function () {
-     document.getElementById('sbmtButton').addEventListener('click', submitForm);
-     document.getElementById('resetButton').addEventListener('click', resetClient);
-     document.getElementById('trigCodeFlowButton').addEventListener('click', trigCodeFlowButton);
-     document.getElementById('clientExists').addEventListener('click', handleClick);
+    document.getElementById('sbmtButton').addEventListener('click', submitForm);
+    document.getElementById('resetButton').addEventListener('click', resetClient);
+    document.getElementById('trigCodeFlowButton').addEventListener('click', trigCodeFlowButton);
+    document.getElementById('logoutButton').addEventListener('click', logout);
   });
 
 })();
